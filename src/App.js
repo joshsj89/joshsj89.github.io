773a761bceffffadc506cb25091a8641665f8c76
joshsj89 <joshsj89@gmail.com>
2023-07-06 19:17:08 -0700

    Added headshot and scroll name event

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
     return (
         <div className="App">
             <NavBar />
+            <div>
+                <div className="intro">
+                    <h1 className="name">Josh Kindarara</h1>
+                    <h2>Software Engineer</h2>
+                    <img 
+                        className="headshot" 
+                        src="images/headshot.jpg"
+                        alt="Headshot" 
+                    />
+                </div>
+            </div>
         </div>
     );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./styles.css";
 
 function NavBar() {
+    // Directly manipulating DOM because it's common for the scroll event
+    useEffect(() => {
+        const handleScroll = () => {
+            const nameHeight = document.querySelector(".name").offsetHeight;
+            const headerTitle = document.querySelector(".header-title");
+
+            if (window.scrollY < nameHeight) {
+                headerTitle.classList.add('hidden');
+                headerTitle.classList.add('unclickable');
+            } else {
+                headerTitle.classList.remove('hidden');
+                headerTitle.classList.remove('unclickable');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
         <nav className="navbar">
             <div className="links">
                 <a href="#about">About</a>
             </div>
-            <a className="header-title" href="/">Josh Kindarara</a>
+            <a 
+                className="header-title hidden unclickable" 
+                href="/"
+                >Josh Kindarara
+            </a>
             <div className="links">
                 <a href="#projects">Projects</a>
             </div>
