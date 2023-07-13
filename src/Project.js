3af9fa71f91f86737b0f3b6d604bd6daae497b19
joshsj89 <joshsj89@gmail.com>
2023-07-13 07:39:28 -0700

    Added links to projects

--- src/Project.js
+++ src/Project.js
@@ -7,7 +7,10 @@ function Project({ project, key }) {
             <img src={project.image} alt={project.name} />
         </div>
         <div className="project-content">
-            <h2 className="project-name">{project.name}</h2>
+            <a 
+                className="project-name"
+                href={project.link}
+                target="_blank" >{project.name}</a>
             <p className="project-description">{project.description}</p>
             <ul className="project-tech">
                 {project.tech.map((tech, index) => {
