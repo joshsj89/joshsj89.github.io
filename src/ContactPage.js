effd7055d4fd2379da28784a7c377f683d66b1ab
joshsj89 <joshsj89@gmail.com>
2023-07-13 04:36:40 -0700

    Moved text on Home Page

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -3,6 +3,7 @@ import ContactForm from './ContactForm';
 function ContactPage(props) {
     return (
         <div className={`${props.className} contact-page`}>
+            <h1>Contact Me</h1>
             <ContactForm />
         </div>
     );
--- src/ProjectsPage.js
+++ src/ProjectsPage.js
@@ -2,7 +2,7 @@ import Project from './Project';
 
 function ProjectsPage(props) {
     return (
-        <div className={`${props.className} projects`}>
+        <div className={`${props.className} projects-page`}>
             <h1>Projects</h1>
             <div className="project-list">
                 {props.projects.map((project, index) => {
