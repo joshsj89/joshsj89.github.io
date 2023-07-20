7002b37668ec234037350efdcd0ed06ff4537df0
joshsj89 <joshsj89@gmail.com>
2023-07-20 06:57:18 -0700

    Resume Button now fetches resume from API

--- src/ResumeButton.js
+++ src/ResumeButton.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
 function ResumeButton() {
-  return (
-    <a href="db/resume.pdf">
-        <button className="resume-button">Resume</button>
-    </a>
-  );
+    const handleClick = async () => {
+        try {
+            const response = await fetch('https://joshsj89-1d7a9e7057c7.herokuapp.com/api/pdf/download/64b92474f706eb41f13dc367');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const resume = await response.json();
+        
+            // YYYY-MM-DDTHH:MM:SS.MSSZ -> YYYY_MM_DD
+            const date = resume.createdAt.slice(0, 10).replace(/-/g, '_');
+
+            const blob = new Blob([resume.data], {type: 'application/pdf'});
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `Josh_Kindarara_Resume_${date}.pdf`;
+            link.click();
+        } catch (error) {
+            console.error('Error Downloading Resume: ', error);
+        }
+    }
+
+    return (
+        <button 
+            className="resume-button"
+            onClick={handleClick}
+            >Resume</button>
+    );
 }
 
 export default ResumeButton;
