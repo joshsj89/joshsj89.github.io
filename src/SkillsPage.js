75e60366f8fc9dd3c52e4eb5ff62e75f61289491
joshsj89 <joshsj89@gmail.com>
2023-07-31 21:40:04 -0700

    Skills now fetched from API

--- src/SkillsPage.js
+++ src/SkillsPage.js
@@ -2,7 +2,7 @@ import Skill from './Skill';
 
 let skills = [];
 try {
-    const response = await fetch('db/skills.json');
+    const response = await fetch('https://joshsj89-1d7a9e7057c7.herokuapp.com/api/skills/all');
     skills = await response.json();
 } catch (err) {
     console.error('Error:', err);
