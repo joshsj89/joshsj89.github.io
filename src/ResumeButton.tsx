b2efd75591862807c1507ccc0a886d883688caff
joshsj89 <joshsj89@gmail.com>
2024-07-15 03:03:07 -0700

    Add progress bar to ResumeButton

--- src/ResumeButton.tsx
+++ src/ResumeButton.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import PDFInterface from './Interfaces/PDFInterface';
 import styles from './ResumeButton.module.css';
 
 function ResumeButton({ downloadID }: { downloadID: string }) {
+    const [progress, setProgress] = useState<number>(0);
+
     const handleClick = async () => {
         try {
             const response = await fetch(`https://joshsj89-1d7a9e7057c7.herokuapp.com/api/pdf/download/${downloadID}`);
@@ -9,6 +12,41 @@ function ResumeButton({ downloadID }: { downloadID: string }) {
                 throw new Error('Network response was not ok');
             }
             const resume: PDFInterface = await response.json();
+
+            const contentLength = resume.data.data.length;
+            let loaded = 0;
+
+            const stream = new ReadableStream({
+                start(controller) {
+                    const reader = new Response(resume.data.data).body?.getReader();
+
+                    const read = async () => {
+                        const nextChunk = await reader?.read();
+
+                        if (nextChunk === undefined) {
+                            controller.close();
+                            return;
+                        }
+
+                        const { done, value } = nextChunk;
+
+                        if (done) {
+                            controller.close();
+                            return;
+                        }
+                        if (value) {
+                            loaded += value.byteLength;
+                            setProgress((loaded / contentLength) * 100);
+
+                            controller.enqueue(value);
+
+                            read();
+                        }
+                    }
+
+                    read();
+                }
+            });
 
             let date: string = '';
         
@@ -19,22 +57,28 @@ function ResumeButton({ downloadID }: { downloadID: string }) {
                 date = resume.createdAt.toISOString().slice(0, 10).replace(/-/g, '_');
             }
 
-            const blob = new Blob([new Uint8Array(resume.data.data)], {type: 'application/pdf'});
+            // const blob = new Blob([new Uint8Array(resume.data.data)], {type: 'application/pdf'});
+            const newResponse = new Response(stream);
+            const blob = await newResponse.blob();
             const url = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
             link.href = url;
             link.download = `Josh_Kindarara_Resume_${date}.pdf`;
             link.click();
+
+            setTimeout(() => { // Reset progress bar after 5 seconds
+                setProgress(0);
+            }, 5000);
         } catch (error) {
             console.error('Error Downloading Resume: ', error);
         }
     }
 
     return (
-        <button 
-            className={styles["resume-button"]}
-            onClick={handleClick}
-            >Resume</button>
+        <div className={styles["resume-button"]} onClick={handleClick}
+        >Resume
+            <span className={styles["progress-bar-fill"]} style={{ width: `${progress}%`}} />
+        </div>
     );
 }
 
