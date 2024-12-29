ee487f0c4011ac86a134cb0ae9dd630e89f3bc46
joshsj89 <joshsj89@gmail.com>
2024-12-29 01:16:43 -0800

    Update index.tsx to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-, document.getElementById('root'));
+// React 17 and earlier
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// , document.getElementById('root'));
+
+// React 18
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
