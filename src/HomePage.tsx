5969f5646ccdaf65bbb2545900430b8ba95a35bc
joshsj89 <joshsj89@gmail.com>
2024-12-30 23:07:39 -0800

    Move About module to HomePage module
    About doesn't meet its original intentions

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,47 @@
-import About from './About';
+import HeadshotInfoBox from './HeadshotInfoBox';
+import { infoTexts } from './Constants/aboutMe';
+import InstagramEmbedder from './InstagramEmbedder';
+import ResumeButton from './ResumeButton';
+import SpotifyEmbedder from './SpotifyEmbedder';
 import './styles.css';
 import styles from './HomePage.module.css';
+import { useName } from './Contexts/NameContext';
 
 function HomePage() {
+    const nameRef = useName();
+
     return (
         <div className={`page ${styles['home-page']}`}>
-            <About />
+            <h1 ref={nameRef} className={styles.name}>Josh Kindarara</h1>
+            <div className={styles['info-boxes']}>
+                <HeadshotInfoBox 
+                    comp={<ResumeButton downloadID="64b92474f706eb41f13dc367" />} 
+                    num="0" 
+                    side="left" 
+                    text={infoTexts['Programming'].text} 
+                    title={infoTexts['Programming'].title} 
+                />
+                <HeadshotInfoBox 
+                    comp={<SpotifyEmbedder size="Compact" sizePercentage={100} type="playlist" uri="1GSNGAu8phHtkUNyHgGyf8" />}
+                    num="2" 
+                    side="right" 
+                    text={infoTexts['Jazz'].text} 
+                    title={infoTexts['Jazz'].title} 
+                />
+                <HeadshotInfoBox 
+                    comp={<InstagramEmbedder account="kinders.photography" height={215} />}
+                    num="3" 
+                    side="left" 
+                    text={infoTexts['Photography'].text} 
+                    title={infoTexts['Photography'].title} 
+                />
+                <HeadshotInfoBox 
+                    num="4" 
+                    side="right" 
+                    text={infoTexts['Football'].text} 
+                    title={infoTexts['Football'].title} 
+                />
+            </div>
         </div>
     );
 }
