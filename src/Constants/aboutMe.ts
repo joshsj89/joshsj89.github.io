41450ab529b44b86bacf276c4f221245c987daf3
Josh Kindarara <joshsj89@gmail.com>
2026-06-23 20:12:26 -0700

    Update aboutMe.ts post-graduation

--- src/Constants/aboutMe.ts
+++ src/Constants/aboutMe.ts
@@ -8,7 +8,7 @@ interface InfoText {
 export const infoTexts: InfoText = {
     "Programming": {
         "title": ["Software Engineer", "Full-Stack Developer"],
-        "text": "Hey, I'm Josh! I am an aspiring software engineer and full-stack developer who loves solving problems and learning new languages, frameworks, and technologies. I am currently a graduate student at Santa Clara University pursuing an M.S. in Computer Science and Engineering with an emphasis in Software Systems and Security. Previously, I graduated from Santa Clara University with a B.S. in Computer Science and Engineering and a minor in Mathematics."
+        "text": "Hey, I'm Josh! I am an aspiring software engineer and full-stack developer who loves solving problems and learning new languages, frameworks, and technologies. Previously, I graduated from Santa Clara University with a B.S. in Computer Science and Engineering and a minor in Mathematics. I then graduated with an M.S. in Computer Science and Engineering with an emphasis in Software Systems."
     },
     "Photography": {
         "title": "Photography",
@@ -16,7 +16,7 @@ export const infoTexts: InfoText = {
     },
     "Jazz": {
         "title": "Jazz",
-        "text": "I love jazz! I play piano and have been learning jazz piano, focusing on learning music theory. I really like stride piano. I also have been playing alto saxophone since 5th grade. I am currently in SCU's Pep Band, performing at sports games and other campus events. Check out my jazz Spotify playlists!"
+        "text": "I love jazz! I play piano and have been learning jazz piano, focusing on learning music theory. I really like stride piano. I also have been playing alto saxophone since 5th grade. I have previously played in SCU's Pep Band, performing at sports games and other campus events. Check out my jazz Spotify playlists!"
     },
     "Football": {
         "title": "Football⚽",
