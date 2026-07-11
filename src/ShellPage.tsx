b37b39bea5d02784f99cb2e2ed12250c12c42651
Josh Kindarara <joshsj89@gmail.com>
2026-07-11 04:04:39 -0700

    Update ShellPage.tsx

--- src/ShellPage.tsx
+++ src/ShellPage.tsx
@@ -128,7 +128,7 @@ Type "help" for more information.`}</pre>
                 <ul className={styles['command-list']}>
                     <li>Implementing support for background jobs (&).</li>
                     <li>Implementing pathname expansion / globbing (e.g., <code>*</code>, <code>?</code>, <code>[...]</code>)</li>
-                    <li>Creating a custom parser for arithmetic expressions rather than relying on Bash's built-in parser.</li>
+                    <li>Creating a custom parser for command substitution and arithmetic expressions rather than relying on Bash through popen().</li>
                     <li>Implementing support for Bash string manipulation (e.g., <code>{'${VAR#pattern}'}</code>, <code>{'${VAR%pattern}'}</code>, etc.)</li>
                     <li>Adding support for more built-in commands (e.g., "help", "jobs", "fg", "bg", "alias", "unalias", etc.).</li>
                     <li>Implementing history expansion functionality (e.g., !n, !!, !string) to allow users to recall and execute previous commands.</li>
